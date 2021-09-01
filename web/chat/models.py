a4b7c433ee079870e2d78fc29d9283797af731e7
Mikhail-Gorelov <>
2021-09-01 09:26:31 +0300

    made interface for chat better

--- web/chat/models.py
+++ web/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-from . import managers
+
+class Message(models.Model):
+    content = models.TextField(max_length=200)
+    date = models.DateTimeField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='author')
--- web/chat/serializers.py
+++ web/chat/serializers.py
@@ -1,3 +1,14 @@
 from rest_framework import serializers
+from . import models
 
-# Create your serializers here.
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Author
+        fields = ['name', 'message']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Message
+        fields = ['content', 'date']
